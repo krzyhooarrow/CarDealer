f71be98cce579bc2f78955aaedaa1692c5d36e28
krzyhooarrow <strzala.krzysztof@interia.pl>
2020-08-24 18:12:09 +0200

    working Zuul communication with Eureka

---
+++ ServiceProxy/src/main/java/spring/Main.java
@@ -0,0 +1,30 @@
+package spring;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+
+@EnableZuulProxy
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {   SpringApplication.run(Main.class, args);   }
+    
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args ->  logger.info("------STARTING APPLICATION------");
+
+    }
+
+
+}
