b24cf7368ca7fa5bf4b56470ad32a9ae278f5be3
krzyhooarrow <strzala.krzysztof@interia.pl>
2020-09-15 22:31:50 +0200

    build script

---
+++ build-script.sh
@@ -0,0 +1,21 @@
+pkill -9 java
+
+cd ServiceDiscovery
+mvn package
+cd target
+nohup java -jar ServiceDiscovery-1.0-SNAPSHOT.jar &
+
+cd ..
+cd ServiceProxy
+mvn package
+cd target
+nohup java -jar  ServiceProxy-1.0-SNAPSHOT.jar &
+
+
+cd ..
+cd TransactionService
+mvn package
+cd target
+nohup spring-boot-0.0.1-SNAPSHOT.jar &
+
+
