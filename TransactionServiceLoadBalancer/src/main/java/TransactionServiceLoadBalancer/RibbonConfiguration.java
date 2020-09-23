218435db41bc537e78fbc812d7dbbaeadcf799d3
krzyhooarrow <strzala.krzysztof@interia.pl>
2020-09-23 14:32:57 +0200

    load balancer

--- TransactionServiceLoadBalancer/src/main/java/TransactionServiceLoadBalancer/RibbonConfiguration.java
+++ TransactionServiceLoadBalancer/src/main/java/TransactionServiceLoadBalancer/RibbonConfiguration.java
@@ -7,45 +7,13 @@ import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@RibbonClients(defaultConfiguration = Config.class)
 public class RibbonConfiguration {
 
-    public static class BazServiceList extends ConfigurationBasedServerList {
+    @Autowired  IClientConfig iClientConfig;
 
-        public BazServiceList(IClientConfig config) {
-            super.initWithNiwsConfig(config);
-        }
-
-    }
-
-}
-
-@Configuration
-class Config {
-
-    @Bean
-    public IRule ribbonRule() {
-        return new BestAvailableRule();
-    }
-
-    @Bean
-    public IPing ribbonPing() {
-        return new PingUrl();
-    }
+    @Bean public IPing ping(IClientConfig iClientConfig) {  return new PingUrl();  }
 
 
-
-    @Bean
-    public ServerList<Server> ribbonServerList(IClientConfig config) {
-        return new RibbonConfiguration.BazServiceList(config);
-    }
-
-    @Bean
-    public ServerListSubsetFilter serverListFilter() {
-        ServerListSubsetFilter filter = new ServerListSubsetFilter();
-        return filter;
-    }
-
-
+    @Bean  public IRule rule(IClientConfig iClientConfig) { return new AvailabilityFilteringRule(); }
 
 }
--- TransactionServiceLoadBalancer/src/main/java/TransactionServiceLoadBalancer/TransactionServiceLoadBalancerApplication.java
+++ TransactionServiceLoadBalancer/src/main/java/TransactionServiceLoadBalancer/TransactionServiceLoadBalancerApplication.java
@@ -1,18 +1,38 @@
 package TransactionServiceLoadBalancer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.http.HttpServletRequest;
 
 @EnableDiscoveryClient
-@RibbonClient(name = "custom", configuration = RibbonConfiguration.class)
-@SpringBootApplication(scanBasePackages={"com.netflix.client.config.IClientConfig"})
+@RibbonClient(name = "server", configuration = RibbonConfiguration.class)
+@SpringBootApplication
+@RestController
 public class TransactionServiceLoadBalancerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TransactionServiceLoadBalancerApplication.class, args);
 	}
 
+	@Autowired private RestTemplate restTemplate;
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate template(){return new RestTemplate();}
+
+	@GetMapping("**")
+	public String invokeServer(HttpServletRequest request){
+		return restTemplate.getForObject("http://transactionservice"+request.getRequestURI(),String.class);}
 }
