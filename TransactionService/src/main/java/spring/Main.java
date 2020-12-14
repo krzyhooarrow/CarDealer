bf980f16981a2473215a0eddc0c45538ee70d18f
krzyhooarrow <strzala.krzysztof@interia.pl>
2020-12-14 16:51:15 +0100

    changes in amazon config

--- TransactionService/src/main/java/spring/Main.java
+++ TransactionService/src/main/java/spring/Main.java
@@ -18,6 +18,10 @@ public class Main {
     public static void main(String[] args) {
         Constants.AMAZON_ID = Arrays.stream(args).filter(arg -> arg.contains("AMAZON_ID")).findFirst().get().replace("AMAZON_ID=","");
         Constants.AMAZON_KEY = Arrays.stream(args).filter(arg -> arg.contains("AMAZON_KEY")).findFirst().get().replace("AMAZON_KEY=","");
+        Constants.AMAZON_ENDPOINT_URL = Arrays.stream(args).filter(arg -> arg.contains("AMAZON_ENDPOINT_URL")).findFirst().get().replace("AMAZON_ENDPOINT_URL=","");
+        Constants.AMAZON_BUCKET_NAME = Arrays.stream(args).filter(arg -> arg.contains("AMAZON_BUCKET_NAME")).findFirst().get().replace("AMAZON_BUCKET_NAME=","");
+        Constants.AMAZON_DIR_NAME = Arrays.stream(args).filter(arg -> arg.contains("AMAZON_DIR_NAME")).findFirst().get().replace("AMAZON_DIR_NAME=","");
+
         Constants.MAILBOX = Arrays.stream(args).filter(arg -> arg.contains("MAILBOX")).findFirst().get().replace("MAILBOX=","");
         Constants.MAILBOX_PASSWORD = Arrays.stream(args).filter(arg -> arg.contains("MAILBOX_PASSWORD")).findFirst().get().replace("MAILBOX_PASSWORD=","");
 
--- TransactionService/src/main/java/spring/web_layer/config/Constants.java
+++ TransactionService/src/main/java/spring/web_layer/config/Constants.java
@@ -5,9 +5,9 @@ public final class Constants {
 
 
     public static final int MAX_IMAGES_PER_OFFER=5;
-    public static final String AMAZON_ENDPOINT_URL="https://s3.eu-west-2.amazonaws.com";
-    public static final String AMAZON_BUCKET_NAME="awsspringbucket";
-    public static final String AMAZON_DIR_NAME="carDealerImages";
+    public static String AMAZON_ENDPOINT_URL;
+    public static String AMAZON_BUCKET_NAME;
+    public static String AMAZON_DIR_NAME;
 
     public static String AMAZON_ID;
     public static String AMAZON_KEY;
